077d9d411a5f27cce0dd167e434faf771318ca61
BozaiAkos <54748729+BozaiAkos@users.noreply.github.com>
2025-02-25 21:10:30 +0100

    Update archive.php

--- content/admin/process/projects/archive.php
+++ content/admin/process/projects/archive.php
@@ -1,5 +1,6 @@
 <?php
 $id = $_POST['id'];
+$project = new Project($id);
 
 if ($stmt = $con->prepare('UPDATE `projects` SET `active` = 0 WHERE `id` = ?')) {
     $stmt->bind_param('i', $id);
@@ -8,5 +9,16 @@ if ($stmt = $con->prepare('UPDATE `projects` SET `active` = 0 WHERE `id` = ?')) 
     }
     $stmt->close();
 }
+
+if ($project->getStatus(1) == 'Befejezett') {
+    $wdate = date('Y-m-d', strtotime('+6 months');
+    if ($stmt = $con->prepare('UPDATE `projects` SET `warranty` = ? WHERE `id` = ?')) {
+    $stmt->bind_param('si', $wdate, $id);
+    if (!$stmt->execute()) {
+        alert_redirect('error', URL . 'admin/projektek/adatlap/d/' . $id);
+    }
+    $stmt->close();
+}
+} 
 
 alert_redirect('success', URL . 'admin/projektek/adatlap/d/' . $id);
